4aec70a7473ac78309d9cec6c33d59bbbc86ca0d
sgeremew <geremews@yahoo.com>
2023-11-13 22:27:29 -0500

    more cleanup

--- src/index.js
+++ src/index.js
@@ -8,14 +8,4 @@ import App from "./App";
 // css
 import './style.css';
 
-// function App() {
-//     return (
-//         <div>
-//             <Header />
-//             <Main />
-//             <Footer />
-//         </div>
-//     );
-// }
-
 ReactDOM.render(<App />, document.getElementById("root"));
